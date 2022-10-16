056c56d2e7796fc473795bf7edae0ded359bebab
Dohyun Kim <00dh.kim@gmail.com>
2022-10-16 19:05:29 +0900

    .

--- encryptor.py
+++ encryptor.py
@@ -1,12 +1,12 @@
 """
 binary encryption by AES-CBC
 
-encoding: utf-8
-key: 32 bytes
-origin: https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+- encoding: utf-8
+- key: 32 bytes
+- origin: https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
 
-date: 2022-10-16
-description:
+- date: 2022-10-16
+- description:
     I modified it to use utf-8 encoding
 
 """
@@ -16,7 +16,7 @@ from secrets import token_bytes
 
 # key = token_bytes(32)
 key = 'happynewyear2022happynewyear2022'.encode('utf-8')
-assert len(key) == 32 # key length must be 32
+assert len(key) == 32 # key length must be 32 bytes
 
 BLOCK_SIZE = 16
 
@@ -48,8 +48,9 @@ if __name__ == '__main__':
     
     plaintext = decrypt(ciphertext)
     
-    print(f'Cipher text: {ciphertext}')
+    print(f'Cipher text: {ciphertext}, length: {len(ciphertext)}')
     if not plaintext:
         print('Message is corrupted')
     else:
-        print(f'Plain text: {plaintext}')
+        len_bytes = len(plaintext.encode('utf-8'))
+        print(f'Plain text: {plaintext}, length: {len_bytes}')
